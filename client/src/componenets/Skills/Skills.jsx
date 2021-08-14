b9384098793e96eaad08c8afa86c4b628fca24d2
Stam743 <spencertam@rocketmail.com>
2021-08-14 16:11:04 -0600

    did Skills, still need to fix spacing a little

--- client/src/componenets/Skills/Skills.jsx
+++ client/src/componenets/Skills/Skills.jsx
@@ -1,19 +1,30 @@
 import Chip from '@material-ui/core/Chip';
-
+import Grid from '@material-ui/core/Grid';
 import "../../App.css";
 
-const AllSkils = ["HTML", "CSS", "Javascript"]
+const AllSkils = ["HTML", "CSS", "Javascript", "C", "C++", "Python", "Java", "C#"]
 
 
 export const Skills = (props) => {
 
     const drawSkills = () => {
-        return AllSkils.map((skill) => { return <Chip label={skill} /> });
+        return AllSkils.map((skill) => { return(
+        <Grid item xs = {6} sm = {4} md = {3} lg = {2}>  
+            <Chip label={skill}/>
+        </Grid>);
+        });
     }
 
     return (
         <div className="section">
-            {drawSkills()}
+            <Grid container spacing = {2}>
+                <Grid item xs = {4}>
+
+                </Grid>
+                <Grid item xs = {4} container spacing = {2}>
+                    {drawSkills()};
+                </Grid>
+            </Grid>
         </div>
     );
 }
