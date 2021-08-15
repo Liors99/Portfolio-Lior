375997e9c655b8507b165848d95512a615ea08f9
Liors99 <47369500+Liors99@users.noreply.github.com>
2021-08-15 13:00:26 -0600

    Added experience section, fixed console warning by adding an index for every mapping operation

---
+++ client/src/componenets/Experience/ExperienceEvent/ExeperienceEvent.jsx
@@ -0,0 +1,71 @@
+import PropTypes from 'prop-types';
+
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+import SchoolIcon from '@material-ui/icons/School';
+import WorkIcon from '@material-ui/icons/Work';
+
+import "./ExperienceEventStyles.css"
+
+export const ExperienceEvent = (props) => {
+
+    const drawIcon = () => {
+        let icon = undefined;
+
+        switch (props.type) {
+            case "education":
+                icon = <SchoolIcon />;
+                break;
+            case "work":
+                icon = <WorkIcon />;
+                break;
+            default:
+                icon = <WorkIcon />;
+                break;
+        }
+
+        return icon;
+
+
+    }
+
+    return (
+        <TimelineItem>
+            <TimelineOppositeContent>
+                <Typography variant="body2" color="textPrimary">
+                    {props.time}
+                </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+                <TimelineDot className="event-icon">
+                    {drawIcon()}
+                </TimelineDot>
+                <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent className="event-content">
+                <Paper elevation={3}>
+                    <h2>
+                        {props.name}
+                    </h2>
+                    <ul>
+                        {props.description.map((desc, i) => { return (<li key={i}> {desc}</li>); })}
+                    </ul>
+                </Paper>
+            </TimelineContent>
+        </TimelineItem>
+    );
+}
+
+ExperienceEvent.propTypes = {
+    time: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.array,
+    type: PropTypes.string.isRequired
+};
--- client/src/componenets/Skills/Skills.jsx
+++ client/src/componenets/Skills/Skills.jsx
@@ -9,23 +9,24 @@ const AllSkils = ["HTML", "CSS", "Javascript", "C", "C++", "Python", "Java", "C#
 export const Skills = (props) => {
 
     const drawSkills = () => {
-        return AllSkils.map((skill) => { return(
-        <Grid item xs = {6} sm = {4} md = {3} lg = {2}>  
-            <Chip label= {skill} className = "skill"/>
-        </Grid>);
+        return AllSkils.map((skill, i) => {
+            return (
+                <Grid item xs={6} sm={4} md={3} lg={2} key={i}>
+                    <Chip label={skill} className="skill" />
+                </Grid>);
         });
     }
 
     return (
         <div className="section">
-                <h1 className = "sectionTitle">Skills</h1>
-            <div className = "skillContainer">
-                <Grid container spacing = {1}>
+            <h1 className="sectionTitle">Skills</h1>
+            <div className="skillContainer">
+                <Grid container spacing={1}>
                     {drawSkills()}
                 </Grid>
             </div>
         </div>
-        
+
 
     );
 }
