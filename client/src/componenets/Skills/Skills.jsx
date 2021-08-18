cda8a118bb632881b728427c216dda5e0595fc52
Stam743 <spencertam@rocketmail.com>
2021-08-18 12:01:30 -0600

    Skills now work with JSON

--- client/src/componenets/Skills/Skills.jsx
+++ client/src/componenets/Skills/Skills.jsx
@@ -1,15 +1,25 @@
 import Chip from '@material-ui/core/Chip';
 import Grid from '@material-ui/core/Grid';
+import { useEffect, useState } from 'react';
 import "../../App.css";
 import "./SkillsStyles.css";
-
-const AllSkils = ["HTML", "CSS", "Javascript", "C", "C++", "Python", "Java", "C#"]
 
 
 export const Skills = (props) => {
 
+    const [skills, setSkills] = useState([]);
+        useEffect(() => {
+            fetch("./Information/Skills.json")
+                .then((res) => {
+                    return res.json()
+                })
+                .then((data) => {
+                    setSkills(data);
+                })
+        }, []);
+
     const drawSkills = () => {
-        return AllSkils.map((skill, i) => {
+        return skills.map((skill, i) => {
             return (
                 <Grid item xs={6} sm={4} md={3} lg={2} key={i}>
                     <Chip label={skill} className="skill" />
